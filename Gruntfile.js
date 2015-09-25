ab5eb834843245605d40840844feb1f63d54717f
adai183 <andreas.daiminger@gmail.com>
2015-09-25 14:07:33 +0200

    Use grunt and <srcset>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,27 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        imagemin: {
-    dynamic: {
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 800,
+            suffix: '_large_1x',
+            quality: 50
+          }]
+        },
         files: [{
-            expand: true,
-            cwd: 'images/',
-            src: ['**/*.{png,jpg,gif}'],
-            dest: 'images/build/'
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
         }]
-    }
-}
+      }
+    },
+  });
 
-    });
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', 'imagemin');
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
 
 };
